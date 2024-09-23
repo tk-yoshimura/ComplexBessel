ab873e56c5bcf5640c80801eb18d2dc6a19ae7ae
tk-yoshimura <yoshimura8192@gmail.com>
2024-09-24 01:54:12 +0900

    fix: test

--- ComplexBesselTests/HankelExpansionTests.cs
+++ ComplexBesselTests/HankelExpansionTests.cs
@@ -99,7 +99,7 @@ namespace ComplexBesselTests {
         }
 
         [TestMethod()]
-        public void BesselJMiniTest() {
+        public void BesselJNu1p25Test() {
             HankelExpansion<Pow2.N4> hankel = new(1.25);
 
             Complex<Pow2.N4>[] expecteds = [
@@ -125,7 +125,7 @@ namespace ComplexBesselTests {
         }
 
         [TestMethod()]
-        public void BesselYMiniTest() {
+        public void BesselYNu1p25Test() {
             HankelExpansion<Pow2.N4> hankel = new(1.25);
 
             Complex<Pow2.N4>[] expecteds = [
@@ -151,7 +151,7 @@ namespace ComplexBesselTests {
         }
 
         [TestMethod()]
-        public void BesselIMiniTest() {
+        public void BesselINu1p25Test() {
             HankelExpansion<Pow2.N4> hankel = new(1.25);
 
             Complex<Pow2.N4>[] expecteds = [
@@ -177,7 +177,7 @@ namespace ComplexBesselTests {
         }
 
         [TestMethod()]
-        public void BesselKMiniTest() {
+        public void BesselKNu1p25Test() {
             HankelExpansion<Pow2.N4> hankel = new(1.25);
 
             Complex<Pow2.N4>[] expecteds = [
@@ -189,6 +189,110 @@ namespace ComplexBesselTests {
                 "-0.00001701900642617936969720732433114038442166-0.00004958223421184402515747294308650095460414i",
                 "-204.2140852044501196181755107589505155153+417.3703365407477352155598375752493218656i",
                 "-0.00001701900642617936969720732433114038442166+0.00004958223421184402515747294308650095460414i"
+            ];
+
+            foreach ((Complex<Pow2.N4> z, Complex<Pow2.N4> expected) in zs_mini.Zip(expecteds)) {
+                Complex<Pow2.N4> actual = hankel.BesselK(z);
+
+                Console.WriteLine(z);
+                Console.WriteLine(expected);
+                Console.WriteLine(actual);
+
+                Assert.IsTrue((actual - expected).Magnitude / expected.Magnitude < 1e-30);
+            }
+        }
+
+        [TestMethod()]
+        public void BesselJNuM1p75Test() {
+            HankelExpansion<Pow2.N4> hankel = new(-1.75);
+
+            Complex<Pow2.N4>[] expecteds = [
+                "-147.2999139438063945323394630815032224390-10.9056339300812418071382865800802817172i",
+                "-147.2999139438063945323394630815032224390+10.9056339300812418071382865800802817172i",
+                "-96.4453203127618438412463729175939687224+111.8682157229589337900597395166247943914i",
+                "-96.4453203127618438412463729175939687224-111.8682157229589337900597395166247943914i",
+                "1.397508963018419728866125463198502317718e26-2.689047103063237008011487557828466479775e26i",
+                "-9.13255376986751184684287116150543446463e25+2.889631506025360495813279339858706432795e26i",
+                "1.397508963018419728866125463198502317718e26+2.689047103063237008011487557828466479775e26i",
+                "-9.13255376986751184684287116150543446463e25-2.889631506025360495813279339858706432795e26i"
+            ];
+
+            foreach ((Complex<Pow2.N4> z, Complex<Pow2.N4> expected) in zs_mini.Zip(expecteds)) {
+                Complex<Pow2.N4> actual = hankel.BesselJ(z);
+
+                Console.WriteLine(z);
+                Console.WriteLine(expected);
+                Console.WriteLine(actual);
+
+                Assert.IsTrue((actual - expected).Magnitude / expected.Magnitude < 1e-30);
+            }
+        }
+
+        [TestMethod()]
+        public void BesselYNuM1p75Test() {
+            HankelExpansion<Pow2.N4> hankel = new(-1.75);
+
+            Complex<Pow2.N4>[] expecteds = [
+                "-10.9056322416899217958437967766265351684+147.2998805626433483749635926306468094384i",
+                "-10.9056322416899217958437967766265351684-147.2998805626433483749635926306468094384i",
+                "111.8681933127851316348824917693777891420+96.4453451106815493494087232567175383028i",
+                "111.8681933127851316348824917693777891420-96.4453451106815493494087232567175383028i",
+                "2.689047103063237008011487557828466479775e26+1.397508963018419728866125463198502317718e26i",
+                "-2.889631506025360495813279339858706432795e26-9.13255376986751184684287116150543446463e25i",
+                "2.689047103063237008011487557828466479775e26-1.397508963018419728866125463198502317718e26i",
+                "-2.889631506025360495813279339858706432795e26+9.13255376986751184684287116150543446463e25i"
+            ];
+
+            foreach ((Complex<Pow2.N4> z, Complex<Pow2.N4> expected) in zs_mini.Zip(expecteds)) {
+                Complex<Pow2.N4> actual = hankel.BesselY(z);
+
+                Console.WriteLine(z);
+                Console.WriteLine(expected);
+                Console.WriteLine(actual);
+
+                Assert.IsTrue((actual - expected).Magnitude / expected.Magnitude < 1e-30);
+            }
+        }
+
+        [TestMethod()]
+        public void BesselINuM1p75Test() {
+            HankelExpansion<Pow2.N4> hankel = new(-1.75);
+
+            Complex<Pow2.N4>[] expecteds = [
+                "-2.62076152242149906566470469518604540524e25-3.019159107207759513123694167620311528728e26i",
+                "-2.62076152242149906566470469518604540524e25+3.019159107207759513123694167620311528728e26i",
+                "1.949552053750027174411954077068820097967e26+2.320183702625431595125833192684946828683e26i",
+                "1.949552053750027174411954077068820097967e26-2.320183702625431595125833192684946828683e26i",
+                "140.2607810578372943130995226449327728200+46.2937446780354935621573347839373412787i",
+                "131.9139702088759482017693824281792414315+66.4447286321608627589382194728601568628i",
+                "140.2607810578372943130995226449327728200-46.2937446780354935621573347839373412787i",
+                "131.9139702088759482017693824281792414315-66.4447286321608627589382194728601568628i"
+            ];
+
+            foreach ((Complex<Pow2.N4> z, Complex<Pow2.N4> expected) in zs_mini.Zip(expecteds)) {
+                Complex<Pow2.N4> actual = hankel.BesselI(z);
+
+                Console.WriteLine(z);
+                Console.WriteLine(expected);
+                Console.WriteLine(actual);
+
+                Assert.IsTrue((actual - expected).Magnitude / expected.Magnitude < 1e-30);
+            }
+        }
+
+        [TestMethod()]
+        public void BesselKNuM1p75Test() {
+            HankelExpansion<Pow2.N4> hankel = new(-1.75);
+
+            Complex<Pow2.N4>[] expecteds = [
+                "-5.35222404565129229070775331686841841211e-30+2.500563373120195349488734686337018462950e-29i",
+                "-5.35222404565129229070775331686841841211e-30-2.500563373120195349488734686337018462950e-29i",
+                "-9.484968071222616258294302283203169493183e26-8.23336514565018365948586434406526860170e25i",
+                "-9.484968071222616258294302283203169493183e26+8.23336514565018365948586434406526860170e25i",
+                "-208.7422488243166520251860480998194073164-414.4199122853585266887857775703335245101i",
+                "-0.00001761577059679953277111031956241000418243-0.00004945855291014150741401368246876143340060i",
+                "-208.7422488243166520251860480998194073164+414.4199122853585266887857775703335245101i",
+                "-0.00001761577059679953277111031956241000418243+0.00004945855291014150741401368246876143340060i"
             ];
 
             foreach ((Complex<Pow2.N4> z, Complex<Pow2.N4> expected) in zs_mini.Zip(expecteds)) {
