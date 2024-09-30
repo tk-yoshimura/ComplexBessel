6ea5e4d3f5f5cea3321a7a082dab78ae6b84b027
tk-yoshimura <yoshimura8192@gmail.com>
2024-09-30 19:55:44 +0900

    fix: test

--- ComplexBesselTests/BesselN4Tests.cs
+++ ComplexBesselTests/BesselN4Tests.cs
@@ -164,18 +164,7 @@ namespace ComplexBesselTests {
                     Console.WriteLine(expected);
                     Console.WriteLine(actual);
 
-                    if (z.R >= z.I * 2 || MultiPrecision<Pow2.N4>.Abs(nu) <= 2 || !MultiPrecision<Pow2.N4>.IsInteger(nu)) {
-                        Assert.IsTrue(err < 1e-33, $"\n{nu}, {z}\n{expected}\n{actual}\n{err}");
-                    }
-                    else if (z.R >= z.I || MultiPrecision<Pow2.N4>.Abs(nu) <= 4) {
-                        Assert.IsTrue(err < 4e-32, $"\n{nu}, {z}\n{expected}\n{actual}\n{err}");
-                    }
-                    else if (z.R >= z.I / 2 || MultiPrecision<Pow2.N4>.Abs(nu) <= 8) {
-                        Assert.IsTrue(err < 2e-30, $"\n{nu}, {z}\n{expected}\n{actual}\n{err}");
-                    }
-                    else {
-                        Assert.IsTrue(err < 8e-29, $"\n{nu}, {z}\n{expected}\n{actual}\n{err}");
-                    }
+                    Assert.IsTrue(err < 1e-34, $"\n{nu}, {z}\n{expected}\n{actual}\n{err}");
                 }
             }
         }
@@ -215,6 +204,45 @@ namespace ComplexBesselTests {
                     Console.WriteLine(actual);
 
                     Assert.IsTrue(err < 1e-34, $"\n{nu}, {z}\n{expected}\n{actual}\n{err}");
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void BesselKExpectedTest() {
+            IEnumerable<string> filepaths = Directory.EnumerateFiles("../../../../mpmath_expected/besselk/", "*.csv");
+
+            foreach (string filepath in filepaths) {
+                string filename = filepath.Split("/").Last();
+                string[] seps = filename.Split("_");
+                MultiPrecision<Pow2.N4> nu = MultiPrecision<Pow2.N4>.Parse(seps[1][2..^4]);
+                using StreamReader sr = new(filepath);
+
+                sr.ReadLine();
+
+                while (!sr.EndOfStream) {
+                    string? line = sr.ReadLine();
+
+                    if (string.IsNullOrEmpty(line)) {
+                        break;
+                    }
+
+                    string[] items = line.Split(",");
+
+                    Complex<Pow2.N4> z = (items[0], items[1]);
+
+                    if (!Complex<Pow2.N4>.TryParse(items[2], null, out Complex<Pow2.N4>? expected)) {
+                        continue;
+                    }
+
+                    Complex<Pow2.N4> actual = BesselN4.BesselK(nu, z);
+                    MultiPrecision<Pow2.N4> err = (expected - actual).Magnitude / expected.Magnitude;
+
+                    Console.WriteLine($"{nu}, {z}");
+                    Console.WriteLine(expected);
+                    Console.WriteLine(actual);
+
+                    Assert.IsTrue(err < 1e-33, $"\n{nu}, {z}\n{expected}\n{actual}\n{err}");
                 }
             }
         }
