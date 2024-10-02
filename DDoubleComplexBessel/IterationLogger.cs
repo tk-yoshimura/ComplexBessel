d6fb758a79caaf5b1ff545a6970116e9f5348f46
tk-yoshimura <yoshimura8192@gmail.com>
2024-10-02 20:36:13 +0900

    add: iteration

---
+++ DDoubleComplexBessel/IterationLogger.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace DDoubleComplexBessel {
+    static class IterationLogger {
+        static readonly Dictionary<string, int> table = [];
+
+        public static void Log(string tag, int n) {
+            if (!table.TryGetValue(tag, out int k)) {
+                table.Add(tag, n);
+
+                Trace.WriteLine($"{tag}: {n}");
+            }
+            else if (k < n) {
+                table[tag] = n;
+
+                Trace.WriteLine($"{tag}: {n}");
+            }
+        }
+    }
+}
