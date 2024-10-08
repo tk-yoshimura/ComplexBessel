137ddfdd021337a955a964f3d571b863c563c896
tk-yoshimura <yoshimura8192@gmail.com>
2024-10-09 03:12:26 +0900

    Revert "fix: y eta coef"
    This reverts commit 73e906c7a4378e763798c4f8010e906ef9c9fb8f.

--- DDoubleOptimizedBessel/RealBessel.cs
+++ DDoubleOptimizedBessel/RealBessel.cs
@@ -1690,18 +1690,7 @@ namespace DDoubleOptimizedBessel {
 
                     for (int i = table.Count; i <= k; i++) {
                         if (alpha != 0d) {
-                            if (ddouble.ILogB(alpha) >= BesselYEpsExponent) {
-                                g = -g * (alpha + i - 1) * (ddouble.Ldexp(alpha, 1) + i - 1d) / (i * (i - alpha));
-                            }
-                            else {
-                                ddouble c0 = checked(i * i - 2 * i + 1);
-                                ddouble c1 = checked(4 * i * i - 5 * i + 1);
-                                ddouble c2 = checked(6 * i * i - 5 * i + 1);
-
-                                ddouble v = alpha / i;
-
-                                g = -g * (c0 + v * (c1 + v * (c2 + v * (c2 + v * (c2 + v * (c2 + v * (c2 + v * (c2 + v * c2)))))))) / (i * i);
-                            }
+                            g = -g * (alpha + i - 1) * (ddouble.Ldexp(alpha, 1) + i - 1d) / (i * (i - alpha));
 
                             ddouble eta = g * (alpha + 2 * i);
 
